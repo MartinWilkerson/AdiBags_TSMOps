0af10219331b96b2979b589dacbfd7ca47bb47fb
Tevildo <drwilko@gmail.com>
2017-02-04 08:20:15 +0000

    remove unused functions

--- AdiBags_TSMOps.lua
+++ AdiBags_TSMOps.lua
@@ -38,19 +38,6 @@ local setFilter = addon:RegisterFilter("TSMOps", 99, 'ABEvent-1.0')
 setFilter.uiName = L['TSMOps']
 setFilter.uiDesc = L['Provide filters for TSM Operations.']
 
-function setFilter:OnInitialize()
-    self.db = addon.db:RegisterNamespace('TSMOps', {
-        profile = {
-            enableMail = true,
-        },
-        char = {},
-    })
-end
-
-function setFilter:Update()
-    self:SendMessage('AdiBags_FiltersChanged')
-end
-
 function setFilter:OnEnable()
     addon:UpdateFilters()
 end
@@ -58,8 +45,6 @@ end
 function setFilter:OnDisable()
     addon:UpdateFilters()
 end
-
-local setNames = {}
 
 function setFilter:Filter(slotData)
     local itemId = slotData.itemId
@@ -72,19 +57,3 @@ function setFilter:Filter(slotData)
     end
 end
 
-function setFilter:GetOptions()
-    return {
-        enableBoE = {
-            name = L['Enable BoE'],
-            desc = L['Check this if you want a section for BoE items.'],
-            type = 'toggle',
-            order = 10,
-        },
-        enableBoA = {
-            name = L['Enable BoA'],
-            desc = L['Check this if you want a section for BoA items.'],
-            type = 'toggle',
-            order = 20,
-        },
-    }, addon:GetOptionHandler(self, false, function() return self:Update() end)
-end
